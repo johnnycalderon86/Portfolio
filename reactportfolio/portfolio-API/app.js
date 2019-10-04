fa83848b19af1fa95a8f6f4ddb93e812c01b4d75
Johnny <johnnycalderondeveloper@gmail.com>
2019-10-04 13:53:38 +0200

    hello

--- reactportfolio/portfolio-API/app.js
+++ reactportfolio/portfolio-API/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const sendGrid = require('@sendgrid/mail');
-const PORT = process.env.PORT || 3030;
+
+
+const sendGrid = require('@sendGrid/mail');
+
+
 const app = express();
 
 
@@ -27,7 +30,7 @@ app.post('/api/email', (req, res, next) => {
 
     console.log(req.body);
 
-    sendGrid.setApiKey('');
+    sendGrid.setApiKey('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');
     const msg = {
         to: 'johnnycalderondeveloper@gmail.com',
         from: req.body.email,
@@ -53,6 +56,74 @@ app.post('/api/email', (req, res, next) => {
         });
 });
 
-
+const PORT = process.env.PORT || 3030;
 app.listen(PORT);
-// 3030, "0.0.0.0"
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const sendGrid = require('@sendgrid/mail');
+// const PORT = process.env.PORT || 3030;
+// const app = express();
+
+
+// app.use(bodyParser.json());
+
+// app.use(cors());
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
+
+
+// app.get('/api', (req, res, next) => {
+//     res.send('API Status: I\'m awesome')
+// });
+
+
+// app.post('/api/email', (req, res, next) => {
+
+//     console.log(req.body);
+
+//     sendGrid.setApiKey('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');
+//     const msg = {
+//         to: 'johnnycalderondeveloper@gmail.com',
+//         from: req.body.email,
+//         subject: 'Website Contact',
+//         text: req.body.message
+//     }
+
+//     sendGrid.send(msg)
+//         .then(result => {
+
+//             res.status(200).json({
+//                 success: true
+//             });
+
+//         })
+//         .catch(err => {
+
+//             console.log('error: ', err);
+//             res.status(401).json({
+//                 success: false
+//             });
+
+//         });
+// });
+
+
+// app.listen(PORT);
+// // 3030, "0.0.0.0"
