be46b581c810cc5579aff7ac06f748caefa0977b
Johnny <johnnycalderondeveloper@gmail.com>
2019-10-04 15:14:02 +0200

    hello

--- reactportfolio/portfolio-API/app.js
+++ reactportfolio/portfolio-API/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
+const PORT = process.env.PORT || 3030;
 const sendGrid = require('@sendGrid/mail');
 
 
@@ -30,9 +29,9 @@ app.post('/api/email', (req, res, next) => {
 
     console.log(req.body);
 
-    sendGrid.setApiKey('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');
+    sendGrid.setApiKey('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');
     const msg = {
-        to: 'johnnycalderondeveloper@gmail.com',
+        to: 'johnnycalderon1986@hotmail.com',
         from: req.body.email,
         subject: 'Website Contact',
         text: req.body.message
@@ -56,74 +55,5 @@ app.post('/api/email', (req, res, next) => {
         });
 });
 
-const PORT = process.env.PORT || 3030;
+
 app.listen(PORT);
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const sendGrid = require('@sendgrid/mail');
-// const PORT = process.env.PORT || 3030;
-// const app = express();
-
-
-// app.use(bodyParser.json());
-
-// app.use(cors());
-
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
-
-
-// app.get('/api', (req, res, next) => {
-//     res.send('API Status: I\'m awesome')
-// });
-
-
-// app.post('/api/email', (req, res, next) => {
-
-//     console.log(req.body);
-
-//     sendGrid.setApiKey('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');
-//     const msg = {
-//         to: 'johnnycalderondeveloper@gmail.com',
-//         from: req.body.email,
-//         subject: 'Website Contact',
-//         text: req.body.message
-//     }
-
-//     sendGrid.send(msg)
-//         .then(result => {
-
-//             res.status(200).json({
-//                 success: true
-//             });
-
-//         })
-//         .catch(err => {
-
-//             console.log('error: ', err);
-//             res.status(401).json({
-//                 success: false
-//             });
-
-//         });
-// });
-
-
-// app.listen(PORT);
-// // 3030, "0.0.0.0"
